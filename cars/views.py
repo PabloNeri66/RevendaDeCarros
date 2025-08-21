711759bce684e0d877ade7c62380f5cd2f9082ef
Pablo Alves Neri <pabloneri66@gmail.com>
2025-08-21 18:46:19 -0300

    Crud Completo Aula70-73

--- cars/views.py
+++ cars/views.py
@@ -3,10 +3,16 @@ from cars.models import Car
 from cars.forms import CarModelForm
 from django.views.generic.list import ListView
 from django.views.generic import CreateView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView, DeleteView
+from django.urls import reverse_lazy
 # Create your views here.
 
 
 class CarsListView(ListView):
+    """
+    Listar Carros
+    """
     model = Car
     template_name = 'cars.html'
     context_object_name = 'cars'
@@ -21,22 +27,36 @@ class CarsListView(ListView):
 
 
 class NewCarCreateView(CreateView):
+    """
+    Criar Carro
+    """
     model = Car
     form_class = CarModelForm
     template_name = 'new_car.html' # ele entende o contexto por form por padrão
     success_url = '/cars/'
 
 
-# class NewCarView(View):
+class CarDetailView(DetailView):
+    """
+    Detalhes do um Carro
+    """
+    model = Car
+    template_name = 'car_detail.html'
 
-#     def get(self, request):
-#         new_car_form = CarModelForm()
-#         return render(request, 'new_car.html', {'new_car_form': new_car_form})
-    
 
-#     def post(self, request):
-#         new_car_form = CarModelForm(request.POST, request.FILES)
-#         if new_car_form.is_valid():
-#             new_car_form.save()
-#             return redirect('cars_list')
-#         return render(request, 'new_car.html', {'new_car_form': new_car_form})
+class CarUpdateView(UpdateView):
+    """
+    Atualizar Carro
+    """
+    model = Car
+    form_class = CarModelForm
+    template_name = 'car_update.html'
+
+    def get_success_url(self):
+        return reverse_lazy('car_detail', kwargs={'pk': self.object.pk})
+
+
+class CarDeleteView(DeleteView):
+    model=Car
+    template_name = 'car_delete.html'
+    success_url = '/cars/'
